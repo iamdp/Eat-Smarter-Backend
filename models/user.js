cdc81f403fb69ea2364ecf7c5256435e4800041d
rmoj <rmoj99@gmail.com>
2018-10-02 22:41:09 -0400

    Create(user.js): added user model

--- models/user.js
+++ models/user.js
@@ -1,9 +1,31 @@
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("user", {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.TEXT,
-    age: DataTypes.INTEGER,
-    caloricGoal: DataTypes.INTEGER
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    email: { type:DataTypes.STRING, allowNull: false, validate: { isEmail: true }},
+    password:{type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [1]
+    }},
+    age: {type:DataTypes.INTEGER,allowNull:false},
+    caloricGoal: {type:DataTypes.INTEGER, allowNull:false}
   });
 
   User.associate = models => {
